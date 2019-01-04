6da049e1ff8935d7381a965e033e88f48cbb1029
Zhou Fang <houshuu@live.jp>
2019-01-05 05:44:06 +0900

    Initial Commit

---
+++ ArduinoEncryptionLock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ArduinoEncryptionLock
+//
+//  Created by Zhou Fang on 2019/01/05.
+//  Copyright © 2019 Zhou Fang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ArduinoEncryptionLock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ArduinoEncryptionLock
+//
+//  Created by Zhou Fang on 2019/01/05.
+//  Copyright © 2019 Zhou Fang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
